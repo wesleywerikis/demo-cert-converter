d80e3e120be99f16a436954d96ed611920abb722
Wesley Werikis <wesleywerikis@outlook.com>
2024-05-24 08:32:37 -0300

    feat [BinaryToPfx] : Added console to better follow the execution process

--- src/main/java/br/com/weswerikis/main/BinaryToPfx.java
+++ src/main/java/br/com/weswerikis/main/BinaryToPfx.java
@@ -1,31 +1,48 @@
 package br.com.weswerikis.main;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class BinaryToPfx {
 
-    public static byte[] readBinaryFile(String filePath) throws IOException {
-        return Files.readAllBytes(Paths.get(filePath));
-    }
+	public static byte[] readBinaryFile(String filePath) throws IOException {
+		System.out.println("Lendo o arquivo binário de: " + filePath);
 
-    public static void writePfxFile(String filePath, byte[] bytes) throws IOException {
-        FileOutputStream fos = new FileOutputStream(new File(filePath));
-        fos.write(bytes);
-        fos.close();
-    }
+		byte[] bytes = Files.readAllBytes(Paths.get(filePath));
 
-    public static void main(String[] args) {
-        try {
-            byte[] binaryBytes = readBinaryFile("C:\\CERTA1\\BINARIO\\CERTIFICADO TESTE.bin");
-            writePfxFile("C:\\CERTA1\\CERT\\CERTIFICADO TESTE.pfx", binaryBytes);
-            System.out.println("Arquivo binário convertido para .pfx com sucesso.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		System.out.println("Conteúdo lido do arquivo binário (em bytes):");
+		for (byte b : bytes) {
+			System.out.print(String.format("%02X ", b));
+		}
+		System.out.println();
+
+		return bytes;
+	}
+
+	public static void writePfxFile(String filePath, byte[] bytes) throws IOException {
+		System.out.println("Escrevendo no arquivo PFX: " + filePath);
+
+		Files.write(Paths.get(filePath), bytes);
+
+		System.out.println("Conteúdo escrito no arquivo PFX (em bytes):");
+		for (byte b : bytes) {
+			System.out.print(String.format("%02X ", b));
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		try {
+			String binFilePath = "C:\\CERTA1\\BINARIO\\CERTIFICADO TESTE.bin";
+			byte[] binaryBytes = readBinaryFile(binFilePath);
+
+			String pfxFilePath = "C:\\CERTA1\\CERT\\CERTIFICADO TESTE.pfx";
+			writePfxFile(pfxFilePath, binaryBytes);
+
+			System.out.println("Arquivo binário convertido para .pfx com sucesso.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
